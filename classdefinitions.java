2d1e6f8619be5d37b3f144c0f4d956d2cd00833f
mzhrby <mracristobal@ucdavis.edu>
2016-02-02 16:55:26 -0800

    Update classdefinitions.java
    added instructions or Part 2
    barebones methods for Sequence
    
    TO-DO: implement Sequence as linkedlist

--- classdefinitions.java
+++ classdefinitions.java
@@ -59,13 +59,37 @@ class Sequence extends Element {
 	//contains what is necessary to produce a list of elements
 	//should include dynamic sizing and other functions mentioned in hw
 	
-	public void first(){
+	Element first(){
 		// returns first element of sequence
-		
+		// pointer to the first and last element
+		// -- implement linked list
+		Element * ptr = ;
+		return 
 	}
 	
-	public void rest(){
-		
+	Sequence rest(){
+		// returns the rest of the elements in the sequence
+		// without creating a new sequence, only pointing to the rest of the orig sequence
 	}
 	
+	int length(){
+		// returns number of elements in Sequence object
+	}
+	
+	void add(Element elm, int pos){
+		// method to add an element in a specified position
+		// if element already exists at pos, elm is inserted at pos
+		// all elements starting at location pos are pushed to the right
+		// if pos is not between 0 and length of Sequence, throw error
+	}
+	
+	void delete(int pos) {
+		// method to remove an element at specified position
+		// after deleting, all elements to the right are pushed to the left
+	}
+	
+	void PrintSequence() {
+		// print sequence by surrounding each sequence with [,]
+		// e.g.: [[1] [2] '3' 'c' [ 1 3 ['4' '5'] ] ]
+	}
 }
