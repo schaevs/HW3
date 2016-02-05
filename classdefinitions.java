f1a7aa63978f84dcab48f5fd6dd85c90d551c0f8
schaevs <aschaevitz15@gmail.com>
2016-02-04 18:23:22 -0800

    part 2 in progress  compiling but not quite working. test prints n1 which is an address?

--- classdefinitions.java
+++ classdefinitions.java
@@ -1,13 +1,26 @@
 import java.util.*;
 
 public class classdefinitions{
-	public static void main(){}
+	public static void main(String[] args){
+		Sequence list = new Sequence();
+
+	    list.add(new MyInteger(),0);
+	    list.add(new MyInteger(),0);
+	    list.add(new MyInteger(),1);
+	    list.add(new MyInteger(),3);
+	    list.add(new MyInteger(),2);
+		
+		list.get(2).printLink();
+	}
 }
 
 abstract class Element{
 	//element content
 	//contain functions shared between char, int, and sequence
 
+	public Element(){
+		
+	}
 	/*public Element Get(){
 		return this;
 	}
@@ -16,15 +29,14 @@ abstract class Element{
 	}*/
 	
 	// Define PRint to be an abstract method
-	abstract void PrintElement(){
-		// System.out.println();
-	}
+	abstract void PrintElement();
 	
 }
 
 class MyChar extends Element {
+	char c;
 	public MyChar(){
-		char c = '0';
+		c = '0';
 	}
 	public char Get(){
 		return this.c;
@@ -32,14 +44,15 @@ class MyChar extends Element {
 	public void Set(char val){
 		this.c = val;
 	}
-	public void PrintChar(char val){
-		System.out.print(val);
+	public void PrintElement(){
+		System.out.print(this.c);
 	}
 }
 
 class MyInteger extends Element {
+	int i;
 	public MyInteger(){
-		int i = 0;
+		i = 0;
 	}
 	public int Get(){
 		return this.i;
@@ -47,9 +60,23 @@ class MyInteger extends Element {
 	public void Set(int val){
 		this.i = val;
 	}			
-	public void PrintInt(int val){
-		System.out.print(val);
+	public void PrintElement(){
+		System.out.print(this.i);
 	}
+}
+
+
+class Link{
+	public Element n1;
+	public Link nextLink;
+	
+	public Link(Element n1arg){
+		n1 = n1arg;
+	}
+	public void printLink() {
+		    System.out.print(n1 + ",");
+	    }
+		
 }
 
 class Sequence extends Element {
@@ -59,14 +86,46 @@ class Sequence extends Element {
 	//contains what is necessary to produce a list of elements
 	//should include dynamic sizing and other functions mentioned in hw
 	
-	Element first(){
+	private Link head;
+	private int sequenceCount;
+	
+	public Sequence(){
+		head = null;
+		sequenceCount = 0;
+	}
+	public void PrintElement(){
+		
+	}
+	
+	/*public void printSequence() {
+		    Link currentLink = head;
+		    System.out.print("List: ");
+		    while(currentLink != null) {
+			    currentLink.printLink();
+			    currentLink = currentLink.nextLink;
+		    }
+		    System.out.println("");
+	}*/
+	
+	/*public void insert(Element n1)){
+		Link link = new Link(n1);
+		link.nextLink = head;
+		head = link;
+	}*/
+	Link get(int index){
+		
+		Link gottenLink=head;
+		for(int i=0; i<index; i++)
+			gottenLink = gottenLink.nextLink;
+		return gottenLink;		
+	}
+	
+	/*Element first(){
 		// returns first element of sequence
 		// pointer to the first and last element
 		// -- implement linked list
-		Element * ptr = ;
-		return 
+		//Element * ptr = ;
 	}
-	
 	Sequence rest(){
 		// returns the rest of the elements in the sequence
 		// without creating a new sequence, only pointing to the rest of the orig sequence
@@ -74,13 +133,21 @@ class Sequence extends Element {
 	
 	int length(){
 		// returns number of elements in Sequence object
-	}
-	
+	}*/
 	void add(Element elm, int pos){
 		// method to add an element in a specified position
 		// if element already exists at pos, elm is inserted at pos
 		// all elements starting at location pos are pushed to the right
 		// if pos is not between 0 and length of Sequence, throw error
+	
+		
+		Link newLink = new Link(elm);
+		newLink.nextLink = this.get(pos);
+		if (this.get(pos-1)==null)
+			head = newLink;
+		else
+			this.get(pos-1).nextLink = newLink;
+		sequenceCount++;
 	}
 	
 	void delete(int pos) {
