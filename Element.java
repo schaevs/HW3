f5b84ac13a672b2edeb37bd90b1a8a0d6a408e9e
mzhrby <mracristobal@ucdavis.edu>
2016-02-08 19:34:28 -0800

    Create Element.java

---
+++ Element.java
@@ -0,0 +1,15 @@
+abstract class Element{
+	//element content
+	//contain functions shared between char, int, and sequence
+
+	public Element(){
+		
+	}
+		
+		char c;
+		int i;
+	
+	// Define PRint to be an abstract method
+	abstract void Print();
+	
+}
